f06c879c43f9cbe906f356446d77b8a7fed7e45f
Alex Kosarevsky <alexkosarevsky@gmail.com>
2020-01-26 23:26:13 +0600

    added urls for app #5

---
+++ todo_project/todolist/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from todolist.views import ListViewSet, TaskViewSet
+
+lists = ListViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+tasks = TaskViewSet.as_view({
+    'get': 'list',
+    'get': 'retrieve',
+    'post': 'create',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+urlpatterns = [
+    path('lists/', lists, name='lists'),
+    path('task-list/<int:pk>/', tasks, name='list'),
+]
